b1ba45ba9e1a4220af9f16922a87f78b121e2e0f
sophias4444 <sophia.syed123@gmail.com>
2021-10-17 19:47:38 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,24 @@
+/**
+* Create a javascript file that includes variables to hold data
+*
+* @link URL
+* @file this file defines the myclass class
+* @author Sophia Syed & Akilah Bussey
+* @since 10.16.21
+*/
+
+// define varibales
+make = "Toyota";
+model = "Sienna LE";
+color = "Silver";
+year = 2016;
+
+// calculate age of car
+age = 2021 - year;
+
+// print output in console
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writelna("Age: " + age + "years<br>");
