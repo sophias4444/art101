74d33b320c74c3894c3c29311f0a28bb5a3dbce3
sophias4444 <sophia.syed123@gmail.com>
2021-11-15 23:19:01 -0800

    lab 12

---
+++ lab12/js/lab.js
@@ -0,0 +1,30 @@
+/*
+* Author: Sophia Syed <sosyed@ucsc.edu>
+* Created: 11.15.21
+* License : Public Domain
+* Lab 12: conditionals
+*/
+function sortingHat(str){
+  var len = str.length;
+  var mod = len % 4;
+  if (mod == 0){
+    return "Gryffindor";
+  }
+  else if (mod == 1){
+    return "Ravenclaw";
+  }
+  else if (mod == 2){
+    return "Slytherin";
+  }
+  else if (mod == 3){
+    return "Hufflepuff";
+  }
+}
+
+var myButton = $("#button");
+myButton.click(function(){
+  var name = $("#input").val();
+  var house = sortingHat(name);
+  var newText = "<p>The House the Sorting Hat has sorted you into is " + house +"</p>";
+  $("#output").html(newText);
+});
