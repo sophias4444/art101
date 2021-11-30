e1bef9b560465a52721ca11e5f7c770115537978
sophias4444 <sophia.syed123@gmail.com>
2021-11-29 21:59:19 -0800

    lab 14/15

---
+++ lab15/js/lab.js
@@ -0,0 +1,25 @@
+var myUrl = "https://catfact.ninja/fact";
+
+$("#activate").click(callAjax);
+
+function callAjax() {
+  console.log("click");
+  // Using the core $.ajax() method
+  $.ajax({
+      // API endpoint
+      url: myUrl,
+      //GET request
+      type: "GET",
+      // data type we expect back
+      dataType : "json",
+  })
+  .done(function(data) {
+      console.log("Success:", data);
+      $("#output").append("<p>" + data.fact + "<br>");
+  })
+  // if the request fails
+  .fail(function(request,error) {
+      console.log(request, error);
+      $("#output").html("Error")
+	})
+}
